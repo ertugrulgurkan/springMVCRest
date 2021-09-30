01cbdb8589050c11a097a2a25a8f43c4023f7f7b
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-09-30 16:46:04 +0300

    category entity changed

--- src/main/java/com/ertugrul/springmvcrest/domain/Category.java
+++ src/main/java/com/ertugrul/springmvcrest/domain/Category.java
@@ -1,17 +1,35 @@
 package com.ertugrul.springmvcrest.domain;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
