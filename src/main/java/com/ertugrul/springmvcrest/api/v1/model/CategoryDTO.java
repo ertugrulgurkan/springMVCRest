f6342fbd0b145238bdb1dc6ae34c128694f042c5
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-09-30 13:42:19 +0300

    Initial commit

---
+++ src/main/java/com/ertugrul/springmvcrest/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package com.ertugrul.springmvcrest.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/test/java/com/ertugrul/springmvcrest/SpringMvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ertugrul.springmvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
